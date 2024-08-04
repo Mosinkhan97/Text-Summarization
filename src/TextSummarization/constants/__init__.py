50b9585b53017acbded7004feb16ef2e3cfb6f61
Mosinkhan97 <mosinkh97@gmail.com>
2024-08-04 16:34:30 +0530

    completed the note book experiment

---
+++ src/TextSummarization/constants/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config\config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+
+
+## C:\Projects\Text-Summarization\config\config.yaml
+## config\config.yaml
